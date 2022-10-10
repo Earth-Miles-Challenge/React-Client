ae77a9204fff2add772d59ae17e443fbc81a1452
Eric Daams <eric@164a.com>
2022-10-10 14:36:37 +0930

    Implemented app render with initial state loading async from Express server.

--- src/index.js
+++ src/index.js
@@ -5,42 +5,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { setupStore } from 'store';
+import { setupStoreWithAsyncState } from 'store';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-import { getProfile } from 'features/users';
+// import { getProfile } from 'features/users';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const profile = getProfile();
-const preloadedState = profile.length
-	? {
-		currentUser: {
-			profile: {
-				first_name: profile.first_name || '',
-				last_name: profile.last_name || '',
-				email: profile.email || '',
-				picture: profile.picture || '',
-				strava_id: profile.strava_id || '',
-			}
-		}
-	}
-	: {};
+const render = async () => {
+	const store = await setupStoreWithAsyncState();
+	root.render(
+		<React.StrictMode>
+			<Router>
+				<Provider store={store}>
+					<I18nextProvider i18n={i18n}>
+						<App/>
+					</I18nextProvider>
+				</Provider>
+			</Router>
+		</React.StrictMode>
+	);
+}
 
-const store = setupStore(preloadedState);
-// const store = setupStore();
-
-root.render(
-	<React.StrictMode>
-		<Router>
-			<Provider store={store}>
-				<I18nextProvider i18n={i18n}>
-					<App/>
-				</I18nextProvider>
-			</Provider>
-		</Router>
-	</React.StrictMode>
-);
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { challengeFormReducer, currentUserReducer, signUpFormReducer } from 'features/users'
+import { challengeFormReducer, currentUserReducer, signUpFormReducer, getProfile } from 'features/users'
 
 const rootReducer = combineReducers({
 	challengeForm: challengeFormReducer,
@@ -12,4 +12,31 @@ export const setupStore = preloadedState => {
 		reducer: rootReducer,
 		preloadedState
 	})
+}
+
+const hydrateProfile = async (preloadedState) => {
+	const _getProfileData = async () => {
+		const profile = await getProfile();
+		return {
+			first_name: profile.first_name || '',
+			last_name: profile.last_name || '',
+			email: profile.email || '',
+			picture: profile.picture || '',
+			strava_id: profile.strava_id || ''
+		}
+	}
+
+	const profileData = await _getProfileData();
+	return {
+		...preloadedState,
+		currentUser: {
+			...preloadedState.currentUser,
+			profile: profileData
+		}
+	};
+}
+
+export const setupStoreWithAsyncState = async () => {
+	const preloadedState = await hydrateProfile({});
+	return setupStore(preloadedState);
 }
