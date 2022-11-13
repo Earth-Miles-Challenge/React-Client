c2251462cee204f8e463829876f8ecef1aec08ef
Eric Daams <eric@164a.com>
2022-11-13 22:32:43 +0930

    Removed commented out code

--- src/features/users/components/profile-form/profile-form.js
+++ src/features/users/components/profile-form/profile-form.js
@@ -55,11 +55,6 @@ export const ProfileForm = props => {
 				<input type="email"
 					id="email"
 					{...register('email', {
-						// pattern: {
-						// 	value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-						// 	message: "invalid email address"
-						// },
-						// pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 						pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 						required: true,
 						value: profile.email,
